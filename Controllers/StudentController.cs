da936cd21c355c5bdb1f6dc7dfcff0db4ab4b241
Student <Student@208-03>
2024-03-05 19:03:48 +0800

    加入專案檔案。

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using _20240305.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20240305.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student s1 = new Student() {
+            Id = 411023057, 
+            Name = "蕭亦真",
+            Department = "資傳系"
+            };
+
+            ViewData["student"] = s1;
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_20240305.MvcApplication" Language="C#" %>
